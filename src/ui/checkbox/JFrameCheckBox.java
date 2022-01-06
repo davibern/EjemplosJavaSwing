7f3182534273fc6f4ce11ef4946d9ad32e3d0adf
davibern <david.bern.pal@gmail.com>
2022-01-06 13:58:01 +0100

    Add checkbox example

--- src/ui/checkbox/JFrameCheckBox.java
+++ src/ui/checkbox/JFrameCheckBox.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ui.checkbox;
 
 /**
  *
- * @author david
+ * @author davibern
+ * @version 1.0
  */
 public class JFrameCheckBox extends javax.swing.JFrame {
 
